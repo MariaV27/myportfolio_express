d772e7ba9f1dc7d42a2b27e8f9d58fa0d1d5aa91
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-04-01 21:16:09 -0400

    added modules

--- js/blog.js
+++ js/blog.js
@@ -19,38 +19,3 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
-
-
-
-$(document).ready(function(){
-
-
-$("#repobutton").on("click", function() {
-  
-  $.ajax({
-  type: "GET",
-  url: "https://api.github.com/users/MariaVintimilla/repos",
-  success: tester
-  });
-
-
-function tester(dataFromRepos){
-  for (var i = 0; i < dataFromRepos.length; i++) {
-    var x = buildElements(dataFromRepos[i].name);
-
-  $("tr").append(x);   
-
-  }
-}
-
-function buildElements(dataRedux) {
-  var y = $("<div>").append(dataRedux);
-  return y;
- }
-
-
-});
-});
-
-
- 
